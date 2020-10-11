9418a58052c2015455fda4b2ac1e7169d4eb7fcb
Trần Đăng Khoa <71996922+fcboy741@users.noreply.github.com>
2020-10-11 23:06:31 +0700

    baitap

---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //drive info
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Drive name: {drive.Name}");
+                    Console.WriteLine($"Format: {drive.DriveFormat}");
+                    Console.WriteLine($"Type: {drive.DriveType}");
+                    Console.WriteLine($"Root directory: {drive.RootDirectory}");
+                    Console.WriteLine($"Volume label: {drive.VolumeLabel}");
+                    Console.WriteLine($"Free space: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Available space:{ drive.AvailableFreeSpace}");
+                    Console.WriteLine($"Total size: {drive.TotalSize}");
+                    Console.WriteLine();
+                }
+            }
+            //xuat ra tat ca file trong document
+            string rootPath = @"C:\Users\Administrator\Documents";
+            string[] dirs = Directory.GetDirectories(rootPath);
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+            //create file txt
+
+            string path = @"C:\Users\Administrator\Desktop\Test.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome to this text file");
+                }
+            }
+
+            // Open the file to read from.           
+            string path1 = @"C:\Users\Administrator\Desktop\Test.txt";
+            using (StreamReader sr = File.OpenText(path1))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
---
+++ Ex6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex6
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            List<Student> stu = new List<Student>();
+            int select;
+            do
+            {
+                Console.WriteLine("1. Add Student\t2. Exit\nYour select: ");
+                select = int.Parse(Console.ReadLine());
+                if (select == 1)
+                {
+                    Student a = new Student();
+                    a.Input();
+                    stu.Add(a);
+                }
+            } while (select != 2);
+
+            Console.WriteLine("\nStudent List =======================");
+            //stu.Sort();
+            stu.Sort(new NameComparer());
+            foreach (Student x in stu)
+            {
+                x.Output();
+            }
+        }
+    }
+}
